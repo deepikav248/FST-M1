7085b5fd368c3cefa6f1559bdfbd28e1d88931bf
deepikav248 <85600448+deepikav248@users.noreply.github.com>
2021-07-06 21:32:36 +0530

    Add files via upload

--- Cucumber/Activities/GoogleSearchSteps.java
+++ Cucumber/Activities/GoogleSearchSteps.java
@@ -1,11 +1,9 @@
-package stepDefinitions;
+package StepDefinition;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import cucumber.api.java.en.And;
@@ -14,34 +12,35 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class GoogleSearchSteps {
-	
-	WebDriver driver;
+    WebDriver driver;
     WebDriverWait wait;
     
     @Given("^User is on Google Home Page$")
     public void userIsOnGooglePage() throws Throwable {
-        //Create a new instance of the Firefox driver
-        driver = new FirefoxDriver();
-        wait = new WebDriverWait(driver, 15);
+        //Create a new instance of the chrome driver
+        System.setProperty("webdriver.chrome.driver","C://Users//DeepikaRani//Downloads//chromedriver_win32//chromedriver.exe");
+        driver = new ChromeDriver();
+        //wait = new WebDriverWait(driver, 15);
                 
         //Open the browser
         driver.get("https://www.google.com");
+        Thread.sleep(5000);
     }
     
     @When("^User types in Cheese and hits ENTER$")
-    public void userTypesInCheeseAndHitsENTER() throws Throwable {
+    public void User_types_in_Cheese_and_hits_ENTER() throws Throwable {
         driver.findElement(By.name("q")).sendKeys("Cheese", Keys.RETURN);
     }
- 
+
     @Then("^Show how many search results were shown$")
-    public void showHowManySearchResultsWereShown() throws Throwable {
-        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.id("result-stats")));
+    public void Show_how_many_search_results_were_shown() throws Throwable {
+        //wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.id("result-stats")));
         String resultStats = driver.findElement(By.id("result-stats")).getText();
         System.out.println("Number of results found: " + resultStats);
     }
- 
+
     @And("^Close the browser$")
-    public void closeTheBrowser() throws Throwable {
+    public void Close_the_browser() throws Throwable {
         driver.close();
     }
 }
